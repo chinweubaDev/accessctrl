4ebf55d0a26cc5b51a01edcccfc1e3e7f6b32e7e
Chinonso Ndukwe <nonsondukwe24@gmail.com>
2025-06-04 15:01:51 +0100

    created the structure

---
+++ app/screens/onboarding/auth/userLoginScreen.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export class userLoginScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>userLoginScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default userLoginScreen;
